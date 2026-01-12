0c962fed9ac9879efacf38c0441d8ca7ed76c0d2
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-12 17:29:02 +0700

    23:13 – ES Modules (ESM)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import getPosts, { getPostsLength } from "./postController.js";
 
-const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+console.log(getPosts());
 
-console.log(`Random number: ${generateRandomNumber()}`);
+console.log(`Post length: ${getPostsLength()}`);
 
-console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
+// const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+// console.log(`Random number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
