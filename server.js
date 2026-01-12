780880f8daaa69e4a413a8a250eb14461a75ebf0
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-12 21:45:35 +0700

    51:52 – Loading Files

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 import http from "http";
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
     try {
+        let filePath;
         if (req.method === 'GET') {
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('<h1>Homepage</h1>');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('<h1>About</h1>');
+                filePath = path.join(__dirname, 'public', 'about.html');
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end('<h1>Not found</h1>');
+                throw new Error('Not found');
             }
         } else {
             throw new Error('Method not allowed');
         }
+
+        const data = await fs.readFile(filePath);
+        res.setHeader('Content-Type', 'text/html');
+        res.write(data);
+        res.end();
     } catch (error) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Server Error');
     }
-
-
 });
 
 server.listen(PORT, () => {
