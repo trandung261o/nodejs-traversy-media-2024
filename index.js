fb690390c7a7932f4bfbaec110d49c2dbb862c84
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-12 17:15:03 +0700

    19:36 – CommonJS Modules

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+
+const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+console.log(`Random number: ${generateRandomNumber()}`);
+
+console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
