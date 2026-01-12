eb6c1dc5dd6ae626b6259709e1cd9a0862e02b32
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-12 19:55:01 +0700

    47:28 – Simple Routing

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    // res.write('Hello world');
-    // res.setHeader('Content-Type', 'text/html');
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>Not found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server Error');
+    }
 
-    // res.statusCode = 404;
 
-    console.log(req.url);
-    console.log(req.method);
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    // res.end('<h1>Hello world</h1>');
-
-    res.end('<h1>Hello world</h1>');
 });
 
 server.listen(PORT, () => {
