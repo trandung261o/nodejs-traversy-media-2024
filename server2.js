5bd17129a1a81e1bd49483155d19b23909ae1a0c
Tran Dinh Dung <trandungtk321@gmail.com>
2026-01-13 17:13:10 +0700

    1:10:27 – Middleware

--- server2.js
+++ server2.js
@@ -8,30 +8,38 @@ const users = [
     { id: 3, name: 'Jim Doe' }
 ];
 
+// Logger middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
 const server = createServer((req, res) => {
-    if (req.url === '/api/users' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(users));
-        res.end();
-    } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'GET') {
-        const id = req.url.split('/')[3];
-        const user = users.find((user) => user.id === parseInt(id));
-        res.setHeader('Content-Type', 'application/json');
+    logger(req, res, () => {
+        if (req.url === '/api/users' && req.method === 'GET') {
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(users));
+            res.end();
+        } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'GET') {
+            const id = req.url.split('/')[3];
+            const user = users.find((user) => user.id === parseInt(id));
+            res.setHeader('Content-Type', 'application/json');
 
-        if (user) {
-            res.write(JSON.stringify(user));
+            if (user) {
+                res.write(JSON.stringify(user));
+            } else {
+                res.statusCode = 404;
+                res.write(JSON.stringify({ message: 'User not found' }));
+            }
+
+            res.end();
         } else {
+            res.setHeader('Content-Type', 'application/json');
             res.statusCode = 404;
-            res.write(JSON.stringify({ message: 'User not found' }));
+            res.write(JSON.stringify({ message: 'Route not found' }));
+            res.end();
         }
-
-        res.end();
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 404;
-        res.write(JSON.stringify({ message: 'Route not found' }));
-        res.end();
-    }
+    })
 });
 
 server.listen(PORT, () => {
